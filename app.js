0c270d1346da329171b4c663389ce279ec698223
kvforero <karenviviana062597@gmail.com>
2026-06-11 17:39:51 -0500

    cambios form
    cambios

--- app.js
+++ app.js
@@ -15,28 +15,4 @@ if (menuBtn && menu) {
   });
 }
 
-const contactoForm = document.getElementById("contactoForm");
-
-if (contactoForm) {
-  contactoForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    const nombre = document.getElementById("nombre").value;
-    const empresa = document.getElementById("empresa").value;
-    const telefono = document.getElementById("telefono").value;
-    const correo = document.getElementById("correo").value;
-    const mensaje = document.getElementById("mensaje").value;
-
-    const texto = `Hola, soy ${nombre}.
-Empresa: ${empresa}
-Teléfono: ${telefono}
-Correo: ${correo}
-Mensaje: ${mensaje}`;
-
-    const url = `https://wa.me/573228667813?text=${encodeURIComponent(texto)}`;
-
-    window.open(url, "_blank");
-  });
-}
-
 console.log("Página Insight Core SAS cargada correctamente.");
